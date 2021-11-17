877ade39fdf0629cce50fab67627467da4257ed5
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2021-11-17 22:48:41 +0100

    Fix #13

---
+++ spec/models/paper_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Paper, type: :model do
+  it "should have title, venue and year" do
+    paper = Paper.new()
+    paper.title = 'Test Paper'
+    paper.venue = 'Potsdam'
+    paper.year = 2021
+    expect(paper.title).to eq('Test Paper')
+    expect(paper.venue).to eq('Potsdam')
+    expect(paper.year).to eq(2021)
+  end
+end
